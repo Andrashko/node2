839cac38bf537794949546f95435ca6c2044d552
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2020-09-30 00:04:51 +0300

    https

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import express from "express";
 import apiRouter from "./api";
 import controllerMongoose from "./api/book/controllerMogoose";
+import fs from "fs";
+import https from "https";
 
-const server = new express();
+
+const server = express();
+
 
 server.use(express.urlencoded());
 server.use(express.json());
 server.use("/api", apiRouter);
-controllerMongoose.connect().then(() => {
-    server.listen(3000);
-    console.log("http://localhost:3000");
+server.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  });
+controllerMongoose.connect().then(() => {       
+    try{
+        const options = {
+            key: fs.readFileSync('./secret/key.pem'),
+            cert: fs.readFileSync('./secret/cert.pem'),
+            ca: fs.readFileSync("./secret/rootCA.pem")
+        };        
+        const httpsServer = https.createServer(options, server);
+        httpsServer.listen(3443);
+        console.log("https://localhost:3443");
+        server.listen(3000);
+        console.log("http://localhost:3000");
+        
+    } catch (e)     {
+        console.error(e);
+    }
 }).catch ((error)=>{
     console.error(error);
 });
+
